24b8151190b3c00f3f08c8d41f1dd020ab5d82c7
jhonnyzp <jhonnyzp@outlook.com>
2017-08-27 23:33:06 -0500

    reconociendo solicitud tipo get

--- src/ServidorWeb.c
+++ src/ServidorWeb.c
@@ -14,7 +14,13 @@ int answer_to_connection (void *cls, struct MHD_Connection *connection, const ch
 	const char *page  = "<html><body>Servidor web Programacion de sistemas</body></html>";
 	struct MHD_Response *response;
 	int ret;
-
+	//procesando solicitud tipo get
+	if (0 == strcmp (method, "GET")){
+	    printf("PROCESANDO SOLICUTUD GET \n");
+	    MHD_get_connection_values (connection, MHD_HEADER_KIND, print_out_key,NULL);
+	    const char* value = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "data");
+	    printf("PROCESANDO value: %s\n",value);
+	 }
 	response = MHD_create_response_from_buffer (strlen (page),(void*) page, MHD_RESPMEM_PERSISTENT);
 	ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
 	MHD_destroy_response (response);
