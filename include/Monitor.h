3488e8b9c8f706cec7dece63d4b176700b186f43
jhonnyzp <jhonnyzp@outlook.com>
2017-09-03 10:29:47 -0500

    funciones adicionacionales para manejo de solicitudes en daemon

--- include/Monitor.h
+++ include/Monitor.h
@@ -1,4 +1,6 @@
 struct udev_device* obtener_hijo(struct udev* udev, struct udev_device* padre, const char* subsistema);
-void enumerar_disp_alm_masivo(struct udev* udev,int logsdaemon);
+char * enumerar_disp_alm_masivo(struct udev* udev);
 void presentar_estructuraMNTENT(const struct mntent *fs);
 void direccionDispositivo(const char *direccion_fisica);
+void escuchandoSolicitudesClientes();
+char* Dispositivo(char *direccion_fisica);
