defea41bf4326a3f38884d227a9196ee1dc3f3cf
jhonnyzp <jhonnyzp@outlook.com>
2017-09-03 10:39:58 -0500

    inicio del archivo servidor_deamon

---
+++ src/servidor_daemon.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>          /* some systems still require this */
+#include <sys/stat.h>
+#include <stdio.h>              /* for convenience */
+#include <stdlib.h>             /* for convenience */
+#include <stddef.h>             /* for offsetof */
+#include <string.h>             /* for convenience */
+#include <unistd.h>             /* for convenience */
+#include <signal.h>             /* for SIG_ERR */ 
+#include <netdb.h> 
+#include <errno.h> 
+#include <syslog.h> 
+#include <sys/socket.h> 
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/resource.h>
+#include <libudev.h>
+#include <mntent.h>
+#include "Monitor.h"
+#define BUFLEN 1024 
+#define BUFFERING 100000
+#define QLEN 50 
+
+#ifndef HOST_NAME_MAX 
+#define HOST_NAME_MAX 256 
+#endif	
+int clfd;  
+int filefd;
+int sockfd;
+int fd;
+
