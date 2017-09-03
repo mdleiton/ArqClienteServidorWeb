f362bea3c523135f728366f4cf473821571e5149
jhonnyzp <jhonnyzp@outlook.com>
2017-09-03 10:41:06 -0500

    Funcion para inicializar el servidor

--- src/servidor_daemon.c
+++ src/servidor_daemon.c
@@ -30,3 +30,21 @@ int filefd;
 int sockfd;
 int fd;
 
+//Funcion para inicializar el servidor
+int initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen){
+	int err = 0;
+	if((fd = socket(addr->sa_family, type, 0)) < 0)
+		return -1;
+	if(bind(fd, addr, alen) < 0)
+		goto errout;
+	if(type == SOCK_STREAM || type == SOCK_SEQPACKET){
+		if(listen(fd, QLEN) < 0)
+			goto errout;
+	}
+	return fd;
+errout:
+	err = errno;
+	close(fd);
+	errno = err;
+	return (-1);
+}
