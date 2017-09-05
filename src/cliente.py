9d546ee4a67a2c490b3ac7a392f06447d39131ea
jhonnyzp <jhonnyzp@outlook.com>
2017-09-05 12:16:50 -0500

    cliente envia json es las solicitudes nombrar dispostivo y escribir archivo

--- src/cliente.py
+++ src/cliente.py
@@ -14,7 +14,7 @@ if sys.argv[3]=="listar_dispositivos":
         print("JSON STATUS:",dic['status'])
         print("LISTADO DE DISPOSITIVO:",dic['dispositivos'])
     else:
-        print("el número de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
+        print("el nÃºmero de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
 elif sys.argv[3]=="nombrar_dispositivo":
     if len(sys.argv) == 6 and sys.argv[2]=="POST":
         url="http://127.0.0.1:"+str(sys.argv[1])+"/"+sys.argv[3]
@@ -27,20 +27,20 @@ elif sys.argv[3]=="nombrar_dispositivo":
         print("JSON RESPUESTA:",dic)
         print("JSON STATUS:",dic['status'])
     else:
-        print("el número de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
+        print("el nÃºmero de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
 elif sys.argv[3]=='leer_archivo':
     if len(sys.argv) == 6 and sys.argv[2]=="GET":
         url="http://127.0.0.1:"+str(sys.argv[1])+"/"+sys.argv[3]
         jsons={"solicitud":sys.argv[3],"nombre": sys.argv[4],"nombre_archivo":sys.argv[5]}
         print(url)
-        r=requests.get(url,data=jsons)
-        dic=r.json()
+        r=requests.get(url,json=jsons)
+        dic=r.text
         print("ENCABEZADOS:",r.headers)
         print("CODIGO ESTADO:",r.status_code)
         print("JSON RESPUESTA:",dic)
-        print("JSON STATUS:",dic['status'])
+        #print("JSON STATUS:",dic['status'])
     else:
-        print("el número de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
+        print("el nÃºmero de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
 elif sys.argv[3]=='escribir_archivo':
     if len(sys.argv) == 6 and sys.argv[2]=="POST":
         url="http://127.0.0.1:"+str(sys.argv[1])+"/"+sys.argv[3]
@@ -51,15 +51,14 @@ elif sys.argv[3]=='escribir_archivo':
         archivo.close()
         jsons={"solicitud":sys.argv[3],"nombre": sys.argv[4],"nombre_archivo":sys.argv[5],"tamano_contenido":len(contenido),"contenido":contenido}
         print(url)
-        r=requests.post(url,data=jsons)
+        r=requests.post(url,json=jsons)
         dic=r.json()
         print("ENCABEZADOS:",r.headers)
         print("CODIGO ESTADO:",r.status_code)
         print("JSON RESPUESTA:",dic)
         print("JSON STATUS:",dic['status'])
     else:
-        print("el número de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
+        print("el nÃºmero de parametros es incorrecto para la solicitud o el metodo para la solicitud es incorrecto")
 else:
     print("solicitud no definida")
-
 
